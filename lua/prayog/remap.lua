428a2346bde9e7abf652b778ac213969dd19a46c
Prayog Paudel <ppaudel93@gmail.com>
2023-07-01 07:59:34 +0545

    Added \p remap for pasting from clipboard

--- lua/prayog/remap.lua
+++ lua/prayog/remap.lua
@@ -11,6 +11,9 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
+vim.keymap.set("n", "<leader>P", [["+P]])
+
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format)
 
