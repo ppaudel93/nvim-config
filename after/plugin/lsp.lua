8df46077fd5e91fb58eb430f0362e283d420cd62
Prayog Paudel <ppaudel93@gmail.com>
2024-04-01 16:00:20 +0545

    Polished up lsp-config, removed deprecated functions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,18 +1,25 @@
 vim.opt.signcolumn = 'yes'
-local lsp = require('lsp-zero')
-
-lsp.preset('recommended')
-lsp.set_preferences({
-    sign_icons = {}
-})
-lsp.nvim_workspace()
-lsp.setup()
+local lsp_zero = require('lsp-zero')
 
 vim.diagnostic.config({
     virtual_text = true,
-    signs = false,
+    signs = true,
     update_in_insert = false,
     underline = true,
     severity_sort = false,
     float = true,
+})
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+  },
 })
