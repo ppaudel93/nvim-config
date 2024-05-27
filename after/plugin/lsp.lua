f27f3652b875f033e761cf93caac743fbf276e34
Prayog Paudel <ppaudel93@gmail.com>
2024-05-27 07:45:39 +0545

    Preserve mappings set to false for LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,7 +13,10 @@ vim.diagnostic.config({
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
+    lsp_zero.default_keymaps({
+        buffer = bufnr,
+        preserve_mappings = false
+    })
 end)
 
 require('mason').setup({})
